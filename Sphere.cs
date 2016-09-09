37159a5c55d4a093598b6d79deaef2302c64b3f8
Ivy Chang <ivycwq@icloud.com>
2016-09-09 09:57:55 -0700

    finishing up the class

---
+++ Sphere.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Sphere {
+  public class Sphere {
+    public int Xcoordinate {get; set;}
+    public int Ycoordinate {get; set;}
+    public int Zcoordinate {get; set;}
+    public double Radius {get; set;}
+    public double CalculateVolume() {
+      return 3/4 * (Math.PI) * (Math.Pow(Radius, 3));
+    }
+    public double CalculateSurface() {
+      return 4 * (Math.PI) * (Math.Pow(Radius, 3));
+    }
+  }
+}
