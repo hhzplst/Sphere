eb04c5efc5a2587fdd70495a7e0b86d5595e7a12
Ivy Chang <ivycwq@icloud.com>
2016-09-14 20:29:24 -0700

    updating test code accordingly

--- SphereTest.cs
+++ SphereTest.cs
@@ -3,15 +3,10 @@
 namespace Sphere{
     public class SphereTest{
         public static void Main(string[] args){
-            Sphere testSphere = new Sphere();
-
-            testSphere.Xcoordinate = 4;
-            testSphere.Ycoordinate = 5;
-            testSphere.Zcoordinate = 2;
+            Sphere testSphere = new Sphere(4,5,2);
             testSphere.Radius = 3.5;
 
-            Console.WriteLine("This is a Sphere at center ({0}, {1}, {2}).", testSphere.Xcoordinate, testSphere.Ycoordinate, testSphere.Zcoordinate);
-            Console.WriteLine("The Sphere has volumn of {0:f} and surface are of {1:f}.", testSphere.CalculateVolume(), testSphere.CalculateSurface());
+            Console.WriteLine(testSphere);
         }
     }
 }
